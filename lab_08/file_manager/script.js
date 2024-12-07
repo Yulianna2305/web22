97e0405a9895f847db31b7899e3be458dcc246d6
Yulianna2305 <pryuliana023@gmail.com>
2024-12-07 12:48:25 +0200

    Delete lab_08/file_manager directory

--- lab_08/file_manager/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.folder').forEach(folder => {
-    folder.addEventListener('click', () => {
-        const target = document.getElementById(folder.dataset.target);
-        target.classList.toggle('hidden');
-    });
-});
